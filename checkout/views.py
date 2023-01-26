704cd61befd5f0bde84236d6022d86e6bf9cf660
mcnic16 <jamesmcnic@yahoo.co.uk>
2023-01-26 15:37:51 +0000

    added to the checkout app for stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
 
+
+import stripe
 
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Your bag is empty")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
