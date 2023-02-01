33222451b9211e5a7efb1e63d8d36ff3625876b2
mcnic16 <jamesmcnic@yahoo.co.uk>
2023-02-01 20:29:40 +0000

    started deployment

---
+++  env.py
@@ -0,0 +1,2 @@
+﻿import os
+os.environ['DATABASE_URL'] = 'postgres://ujuukznd:xYEtlCDoaRQcXHMvjB_il9293aYo-y4B@kandula.db.elephantsql.com/ujuukznd'
--- sister_resin_craft/settings.py
+++ sister_resin_craft/settings.py
@@ -13,6 +13,8 @@ import os
 import dj_database_url
 from pathlib import Path
 
+if os.path.isfile("env.py"):
+    import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,11 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=*38r&6l2@)vkb0vf=gx*l1(r(c36c&+r-)!!b($i_0yzwhto_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
 
 
+ALLOWED_HOSTS = ['mcnic16-sister-resin-craft.herokuapp.com', 'localhost']
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 # Application definition
 
 INSTALLED_APPS = [
@@ -118,14 +121,21 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+
 
 
 # Password validation
