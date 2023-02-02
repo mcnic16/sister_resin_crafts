5abdf45dd20f669b9616705a7bc67269ce087272
mcnic16 <jamesmcnic@yahoo.co.uk>
2023-02-02 08:43:31 +0000

    removed secret key and set debug

--- sister_resin_craft/settings.py
+++ sister_resin_craft/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=*38r&6l2@)vkb0vf=gx*l1(r(c36c&+r-)!!b($i_0yzwhto_'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 
 ALLOWED_HOSTS = ['sister-resin-craft.herokuapp.com', 'localhost']
